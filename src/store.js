0ecd2ca5774957e51d733195275c22be4a3b1a8b
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-02-11 21:58:47 -0600

    Set page title in state

--- src/store.js
+++ src/store.js
@@ -1,16 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    title: "CBCode"
   },
-  mutations: {
-
-  },
-  actions: {
-
-  }
-})
+  mutations: {},
+  actions: {}
+});
