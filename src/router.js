e4bcca53c5de74c5711d21093a5cd6babd19efa3
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-02-13 14:00:45 -0600

    Update by add a new route to course list

--- src/router.js
+++ src/router.js
@@ -1,23 +1,27 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import Courses from "./views/Courses.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: "/about",
+      name: "about",
+      component: About
+    },
+    {
+      path: "/courses",
+      name: "courses",
+      component: Courses
     }
   ]
-})
+});
