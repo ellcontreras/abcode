20374bcbed868b062e8e55cc8a6c14404033b98a
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-02-12 22:40:08 -0600

    Created Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered has-text-white">
+      <p>
+        <strong class="has-text-white">CBCode</strong> by
+        <a href="https://github.com/Obsinqsob01" target="__blank">Luis Contreras</a>. The source code is licensed
+        <a
+          href="http://opensource.org/licenses/mit-license.php"
+          target="__blank"
+        >MIT</a>.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+a {
+  color: #4681df !important;
+}
+
+a:hover {
+  color: whitesmoke !important;
+}
+</style>
+
