1c6a1d748c888958bd6b343e4aa22a501dd8eb56
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-02-13 14:00:16 -0600

    Created navbar component and refactor navbar in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,52 @@
 <template>
   <div>
-    <div class="navbar is-primary">
-      <div class="navbar-brand" @click="$router.push('/')">
-        <h3 id="title" class="subtitle is-3 has-text-white navbar-item">{{ $store.state.title }}</h3>
+    <div class="hero background-gradient is-bold" v-if="$route.name === 'home'">
+      <div class="hero-head">
+        <navbar/>
+      </div>
+      <div class="hero-body">
+        <h1 class="title is-1 has-text-centered has-text-white">Aprende programación desde cero!</h1>
+        <p
+          class="has-text-centered has-text-white"
+        >Selecciona tu ruta de aprendizaje de acuerdo a tu semestre</p>
+
+        <!-- <div class="columns">
+          <div class="column">
+            <img src="./assets/c_icon.svg" alt="">
+          </div>
+          <div class="column">
+            <img src="./assets/c_sharp.svg" alt="">
+          </div>
+          <div class="column">
+            <img src="./assets/html_icon.svg" alt="">
+          </div>
+          <div class="column">
+            <img src="./assets/android_icon.svg" alt="">
+          </div>
+        </div>-->
       </div>
     </div>
-    <div class="container">
-      <Home/>
+    <div v-else>
+      <navbar/>
     </div>
+    <div :class="{'container is-fluid' : $route.name !== 'home'}">
+      <router-view/>
+    </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Home from "./views/Home";
+import Home from "./views/Home.vue";
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "App",
   components: {
-    Home
+    Home,
+    Navbar,
+    Footer
   },
   data() {
     return {
---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navbar" :class="{'margin-bottom' : $route.name !== 'home'}">
+    <div class="navbar-brand" @click="$router.push({name: 'home'})">
+      <h3 id="title" class="subtitle is-3 has-text-white navbar-item">{{ $store.state.title }}</h3>
+    </div>
+    <div class="navbar-end">
+      <div class="navbar-menu">
+        <div class="navbar-item">
+          <router-link to="/courses">Cursos</router-link>
+        </div>
+        <div class="navbar-item">
+          <router-link to="/about">Acerca</router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
