6e0cd157ffb21ee8d8bbd25992380d28c27996ca
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-02-22 17:26:27 -0600

    Created the view to display all the courses

---
+++ src/views/Courses.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="hero background-gradient-secondary">
+      <div class="hero-body">
+        <h1 class="title is-1 has-text-centered has-text-white">Todos los cursos</h1>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="hero" v-for="(course, key) of $store.state.courses" :key="key">
+        <div class="hero-body">
+          <div class="media">
+            <div class="media-left">
+              <img :src="course.image" class="image is-96x96" :alt="course.image">
+            </div>
+            <div class="media-content">
+              <h3 class="subtitle is-3">{{ course.title }}</h3>
+              <p>{{ course.description }}</p>
+            </div>
+          </div>
+        </div>
+        <div class="hero-foot">
+          <div class="columns">
+            <div v-for="(link, k) of course.links" :key="k">
+              <div class="column">
+                <router-link :to="`/course/${link}`">{{ link }}</router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Courses",
+  data: () => ({
+    prefix: "../static/"
+  })
+};
+</script>
+
+
+<style scoped>
+a {
+  color: #4681df !important;
+}
+
+a:hover {
+  color: #444 !important;
+}
+</style>
