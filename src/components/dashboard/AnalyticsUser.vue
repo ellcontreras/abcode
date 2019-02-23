9ca1504da4de429640b92f9ca6e7d12979bffda6
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-02-23 15:51:51 -0600

    Created the folder to host all the dasboard components

---
+++ src/components/dashboard/AnalyticsUser.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="tile is-ancestor">
+    <div class="tile notification is-primary is-child">
+      <h3 class="subtitle is-3">Total de Usuarios</h3>
+      <h2 class="subtitle is-2 has-text-centered">5</h2>
+    </div>
+    <div class="tile notification is-success is-child">
+      <h3 class="subtitle is-3">Usuarios Activos</h3>
+      <h2 class="subtitle is-2 has-text-centered">5</h2>
+    </div>
+    <div class="tile notification is-danger is-child">
+      <h3 class="subtitle is-3">Usuarios que han terminado cursos</h3>
+      <h2 class="subtitle is-2 has-text-centered">5</h2>
+    </div>
+  </div>
+</template>
