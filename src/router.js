c7aa3db73c24ce942265205c3df59d3b3e888b0a
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-02-23 15:56:01 -0600

    Add dashboard route

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
 import Courses from "./views/Courses.vue";
+import Dashboard from "./views/Dashboard.vue";
 
 Vue.use(Router);
 
@@ -22,6 +23,11 @@ export default new Router({
       path: "/courses",
       name: "courses",
       component: Courses
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: Dashboard
     }
   ]
 });
