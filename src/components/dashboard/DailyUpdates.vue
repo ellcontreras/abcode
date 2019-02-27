cdfad3f8106388c27a7bb90df3859548e1109c71
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-02-27 12:40:36 -0600

    Refactor daily updates

---
+++ src/components/dashboard/DailyUpdates.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h2 class="title is-2 has-text-centered">Dashboard</h2>
+    <div class="card">
+      <div class="card-content">
+        <h3 class="subtitle is-3">Actualizaciones diarias</h3>
+        <div id="scroll">
+          <div class="card" v-for="(update, key) of $store.state.dailyUpdates" :key="key">
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <span
+                    class="tag is-black"
+                    @click="$store.commit('ADD_DAILY_UPDATE', 'hola');"
+                  >Notificación</span>
+                  <p>12:05 p.m</p>
+                </div>
+                <div class="media-content">
+                  <router-link :to="`/update/${update}`">{{ update }}</router-link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #444 !important;
+}
+a:hover {
+  color: #111 !important;
+}
+#scroll {
+  max-height: 292px;
+  overflow: auto;
+}
+</style>
+
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,57 +4,25 @@
       <Menu/>
     </div>
     <div class="column">
-      <h2 class="title is-2 has-text-centered">Dashboard</h2>
-      <div class="card">
-        <div class="card-content">
-          <h3 class="subtitle is-3">Actualizaciones diarias</h3>
-          <div id="scroll">
-            <div class="card" v-for="(update, key) of $store.state.dailyUpdates" :key="key">
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-left">
-                    <span
-                      class="tag is-black"
-                      @click="$store.commit('ADD_DAILY_UPDATE', 'hola');"
-                    >Notificación</span>
-                    <p>12:05 p.m</p>
-                  </div>
-                  <div class="media-content">
-                    <router-link :to="`/update/${update}`">{{ update }}</router-link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div v-if="$route.name === 'dashboard'">
+        <daily-updates/>
       </div>
+      <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
 import AnalyticsUser from "@/components/dashboard/AnalyticsUser.vue";
+import DailyUpdates from "@/components/dashboard/DailyUpdates.vue";
 import Menu from "@/components/dashboard/Menu.vue";
 
 export default {
   name: "Dashboard",
   components: {
     AnalyticsUser,
-    Menu
+    Menu,
+    DailyUpdates
   }
 };
 </script>
-
-<style scoped>
-a {
-  color: #444 !important;
-}
-a:hover {
-  color: #111 !important;
-}
-#scroll {
-  max-height: 292px;
-  overflow: auto;
-}
-</style>
-
