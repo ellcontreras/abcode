366b8f769e00894baf48e6acbfd7f8e4009d4cee
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-02-26 22:23:31 -0600

    Updated by add the user info

---
+++ src/components/dashboard/Menu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hero is-primary">
+    <div class="hero-body">
+      <figure class="image container is-128x128">
+        <img
+          src="https://amp.businessinsider.com/images/5899ffcf6e09a897008b5c04-750-750.jpg"
+          alt="profile image"
+        >
+      </figure>
+
+      <p class="has-text-centered">
+        <b>My name</b>
+      </p>
+      <aside class="menu">
+        <p class="menu-label">General</p>
+        <ul class="menu-list">
+          <li>
+            <router-link to="/dashboard">Dashboard</router-link>
+          </li>
+        </ul>
+        <p class="menu-label">Courses</p>
+        <ul class="menu-list">
+          <li>
+            <router-link to="/dashboard/courses">Cursos</router-link>
+          </li>
+        </ul>
+      </aside>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.menu-label {
+  color: whitesmoke !important;
+}
+</style>
