966638a436a5d4ff55901ca37c7edbd7922d2be4
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-02-26 22:22:27 -0600

    Created the component that will display all the courses

---
+++ src/components/dashboard/CourseList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2 class="subtitle is-2">Cursos</h2>
+    <div class="scrolling-wrapper">
+      <div v-for="(course, key) of $store.getters.courses" :key="key" class="scrolling-card">
+        <div :class="$store.getters.categoryColor(course.category).color" class="notification">
+          <h3 class="subtitle is-3">{{ course.name }}</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
