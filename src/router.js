8019213ce63f89c40eaef134f0ca59e40bcccc00
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-02-26 21:23:07 -0600

    Created the 404 error handler

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Home from "./views/Home.vue";
 import About from "./views/About.vue";
 import Courses from "./views/Courses.vue";
 import Dashboard from "./views/Dashboard.vue";
+import PageNotFound from "./views/PageNotFound.vue";
 
 Vue.use(Router);
 
@@ -28,6 +29,11 @@ export default new Router({
       path: "/dashboard",
       name: "dashboard",
       component: Dashboard
+    },
+    {
+      path: "*",
+      name: "404",
+      component: PageNotFound
     }
   ],
   linkActiveClass: "is-active"
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="hero is-large is-success">
+    <div class="hero-body">
+      <h1 class="title is-1 has-text-centered">Error 404</h1>
+      <h2 class="subtitle is-2 has-text-centered">La página solicitada no ha sido encontrada.</h2>
+    </div>
+  </div>
+</template>
