1d573f3c5aa69d10aeee58ee01110b4ac6f14b17
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-03-07 15:29:21 -0600

    Render all the courses

--- src/components/dashboard/CourseList.vue
+++ src/components/dashboard/CourseList.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>
-    <h2 class="subtitle is-2">Cursos</h2>
-    <div class="scrolling-wrapper">
-      <div v-for="(course, key) of $store.getters.courses" :key="key" class="scrolling-card">
-        <div :class="$store.getters.categoryColor(course.category).color" class="notification">
-          <h3 class="subtitle is-3">{{ course.name }}</h3>
-        </div>
+  <div class="container">
+    <p>
+      <b>Selecciona un curso para editarlo:</b>
+    </p>
+    <br>
+    <div v-for="(course, key) of $store.getters.coursesPaginated" :key="key" class="columns">
+      <div
+        v-for="(c, k) of course"
+        :key="k"
+        class="column hero"
+        :class="$store.getters.categoryColor(c.category).color"
+      >
+        <router-link :to="`/dashboard/course/edit/${c.id}`">
+          <div class="hero-body">
+            <h3 class="subtitle is-3 has-text-centered">{{ c.name }}</h3>
+          </div>
+        </router-link>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.hero {
+  margin: 0.5em;
+}
+</style>
+
