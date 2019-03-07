dc5f27bc71fadef208f8510b1a848a76b2d5767b
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-03-07 15:27:41 -0600

    Added new styles

--- src/views/DashboardCourses.vue
+++ src/views/DashboardCourses.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
     <h1 class="title is-1 has-text-centered">Courses</h1>
+
+    <router-link to="/dashboard/course/new">
+      <button class="button is-rounded is-outlined is-primary">Agregar Curso</button>
+    </router-link>
+    <hr>
+    <course-list/>
   </div>
 </template>
+
+<script>
+import CourseList from "@/components/dashboard/CourseList.vue";
+
+export default {
+  name: "DashboardComponents",
+  components: {
+    CourseList
+  }
+};
+</script>
+
+<style scoped>
+a {
+  color: #444 !important;
+}
+a:hover {
+  color: #445 !important;
+}
+</style>
