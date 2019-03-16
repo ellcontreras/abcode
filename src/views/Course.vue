a1b3214c5b56be75280dd0bf7c53272e8a9730de
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-03-16 00:33:59 -0600

    Vuex api call to get a course in store

--- src/views/Course.vue
+++ src/views/Course.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="section">
     <span class="tag is-link">{{ course.Category }}</span>
-    <h2 class="subtitle is-2  has-text-center">{{ course.Name }}</h2>
+    <h2 class="subtitle is-2 has-text-center">{{ course.Name }}</h2>
     <div v-html="course.Description"></div>
   </div>
 </template>
@@ -11,17 +11,15 @@ import { getCourseById } from "@/services/course";
 
 export default {
   name: "Course",
-  data: () => ({
-    course: []
-  }),
-  beforeMount() {
-    getCourseById(this.$route.params.id)
-      .then(res => {
-        this.course = res.data;
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  created() {
+    if (!this.course.Name) {
+      this.$store.dispatch("courseById", this.$route.params.id);
+    }
+  },
+  computed: {
+    course() {
+      return this.$store.getters.courseById(this.$route.params.id);
+    }
   }
 };
 </script>
