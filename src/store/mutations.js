98d7aa4b03b96492527e4692c3d7ff8af51c76a1
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-03-16 00:31:12 -0600

    Added api url base

--- src/store/mutations.js
+++
@@ -1,7 +0,0 @@
-import { ADD_DAILY_UPDATE, UPDATE_COURSE } from "./mutation-types";
-
-export const DailyUpdatesMutations = {
-  [ADD_DAILY_UPDATE](state, payload) {
-    state.dailyUpdates.push(payload);
-  }
-};
