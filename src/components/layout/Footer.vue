0bfaaeb9a03d181d4134b9462b450f48f81d556e
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-03-28 08:33:32 -0600

    Updated footer to use more space

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="my-footer">
+    <el-row>
+      <el-col :span="8">
+        <p>
+          <router-link to="/">Inicio</router-link>
+        </p>
+        <p>
+          <router-link to="/about">Acerca</router-link>
+        </p>
+      </el-col>
+      <el-col :span="8">
+        <div>
+          <p>
+            <router-link to="/courses">Cursos</router-link>
+          </p>
+          <p v-for="course in $store.getters.allCourses" :key="course.Id">
+            <router-link to="/courses">{{ course.Name }}</router-link>
+          </p>
+        </div>
+      </el-col>
+
+      <el-col :span="8">
+        <div class="has-text-centered">
+          <h1>{{ $store.state.title }}</h1>
+        </div>
+      </el-col>
+    </el-row>
     <div class="has-text-centered">
       <p>
         <strong>{{ $store.state.title }}</strong> by
