5840a34ea027758a9b829f071d1af5214eeb3c89
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-04-17 14:24:17 -0500

    Updated quiz state from object to array

--- src/store/store.js
+++ src/store/store.js
@@ -36,7 +36,7 @@ export default new Vuex.Store({
     temasByCourse: [],
     lessonsByTema: [],
     course: {},
-    quiz: {},
+    quiz: [],
     courseCategories: [
       {
         title: "Fundamentos de Programación",
